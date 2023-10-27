22ab086fdeb99e380ebeaf5aa9a7b5d52a0b974d
John Folliard <johnfolliard3@yahoo.com>
2023-10-27 01:50:11 -0700

    create packet decoder helper type

--- PacketSerial/main.py
+++ PacketSerial/main.py
@@ -30,29 +30,47 @@ calculator = Calculator(Configuration(
         polynomial=0x8001
     ))
 
-running_buffer = []
-while True:
-    if arduino.is_open and arduino.in_waiting > 0:
-        in_buffer = [i for i in arduino.read(arduino.in_waiting)]
-        for b in in_buffer:
+class PacketDecoder:
+    def __init__(self, max_buffer_size=128):
+        self.running_buffer = []
+        self.max_buffer_size = max_buffer_size
+
+    def consume(self, buffer: bytes):
+        packets = []
+        for b in buffer:
             if b == 0:
                 try:
-                    packet = cobs.decode(bytes(running_buffer))
-                    messageType, sequence, buttonState, encoder0Value, encoder1Value, msgCrc = \
-                        struct.unpack('>HHHhhH', packet)
-
-                    payloadCrc = calculator.checksum(bytes(packet[:-2]))
-                    if payloadCrc == msgCrc:
-                        print(f'0x{messageType:02X}: {sequence} {buttonState} {encoder0Value} {encoder1Value}')
+                    packet = cobs.decode(bytes(self.running_buffer))
+                    packets.append(packet)
                 except:
+                    # failed to decode (could have just connected or maybe invalid data)
                     pass
-                running_buffer = []
+                self.running_buffer = []
             else:
                 # add to running queue
-                running_buffer.append(b)
-                if len(running_buffer) > 32:
-                    running_buffer.pop(0)
+                self.running_buffer.append(b)
+                if len(self.running_buffer) > self.max_buffer_size:
+                    self.running_buffer.pop(0)
+        return packets
+
+decoder = PacketDecoder()
+while True:
+    if arduino.is_open and arduino.in_waiting > 0:
+        packets = decoder.consume(arduino.read(arduino.in_waiting))
+        for packet in packets:
+            try:
+                # unpack the message
+                messageType, sequence, buttonState, encoder0Value, encoder1Value, msgCrc = \
+                    struct.unpack('>HHHhhH', packet)
+
+                # confirm CRC matches payload
+                payloadCrc = calculator.checksum(bytes(packet[:-2]))
+                if payloadCrc == msgCrc:
+                    # TODO got an ButtonReportMsg; process it
+                    print(f'0x{messageType:02X}: {sequence} {buttonState} {encoder0Value} {encoder1Value}')
+            except:
+                # failed to decode the message
+                pass
 
     # target 50hz
     time.sleep(0.02)
-    
