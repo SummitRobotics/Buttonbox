5a55c37e7988c2d51748f589484e3bfc10d2bd6d
John Folliard <johnfolliard3@yahoo.com>
2024-02-02 16:31:34 -0800

    update

--- FinalDesign/FinalDesign.ino
+++ FinalDesign/FinalDesign.ino
@@ -1,7 +1,6 @@
 #include <Encoder.h>
 #include <Joystick.h>
 
-
 // Pin Layout
 #define ENCODER_1_PIN_1 3
 #define ENCODER_1_PIN_2 2
@@ -22,10 +21,7 @@
 #define MIDDLE_COLUMN_BUTTON_LADDER A4
 #define RIGHT_COLUMN_BUTTON_LADDER A5
 
-#define FUN_DIAL_LADDER A2
-
-#define MISSILE_SWITCH_1 13
-#define MISSILE_SWITCH_2 A1
+#define FUN_MISSLE_LADDER A2
 
 Encoder enc1(ENCODER_1_PIN_1, ENCODER_1_PIN_2);
 Encoder enc2(ENCODER_2_PIN_1, ENCODER_2_PIN_1);
@@ -67,37 +63,58 @@ int near(int val, int ref, int dist = 4) { return abs(val - ref) <= dist; }
  * This function reads the given analog pin and converts to a 3-bit digital
  * value.
  */
-int left_column_ladder_table[8] = { 
-  0,    // 000
-  515,  // 001
-  616,  // 010
-  770,  // 011
-  682,  // 100
-  820,  // 101
-  829,  // 110
-  897   // 111
+int left_column_ladder_table[8] = {
+    0,   // 000
+    515, // 001
+    616, // 010
+    770, // 011
+    682, // 100
+    820, // 101
+    829, // 110
+    897  // 111
 };
 
-int middle_column_ladder_table[8] = { 
-  0,    // 000
-  686,  // 001
-  617,  // 010
-  831,  // 011
-  512,  // 100
-  821,  // 101
-  770,  // 110
-  898   // 111
+int middle_column_ladder_table[8] = {
+    0,   // 000
+    686, // 001
+    617, // 010
+    831, // 011
+    512, // 100
+    821, // 101
+    770, // 110
+    898  // 111
 };
 
-int right_column_ladder_table[8] = { 
-  0,    // 000
-  685,  // 001
-  617,  // 010
-  831,  // 011
-  512,  // 100
-  821,  // 101
-  770,  // 110
-  897   // 111
+int right_column_ladder_table[8] = {
+    0,   // 000
+    685, // 001
+    617, // 010
+    831, // 011
+    512, // 100
+    821, // 101
+    770, // 110
+    897  // 111
+};
+
+#define MISSILE_SWITCH_1_MASK 0x1
+#define MISSILE_SWITCH_2_MASK 0x2
+#define FUN_DIAL_LEFT_MASK 0x4
+#define FUN_DIAL_RIGHT_MASK 0x8
+
+// FUN_DIAL_LEFT + FUN_DIAL_RIGHT not possible
+int fun_missle_ladder_table[12] = {
+    0,   // 0000
+    407, // 0001
+    555, // 0010
+    635, // 0011
+    176, // 0100
+    407, // 0101
+    505, // 0110
+    602, // 0111
+    88,  // 1000
+    362, // 1001
+    527, // 1010
+    617, // 1011
 };
 
 int readLadderPin(int pin, int num_entries, int table[]) {
@@ -120,6 +137,7 @@ void loop() {
   int right_column_buttons =
       readLadderPin(RIGHT_COLUMN_BUTTON_LADDER, 8, right_column_ladder_table);
 
+
   // Light up pins based on ladder bits set above
   digitalWrite(BUTTON_LED_1, left_column_buttons & 0x1 ? HIGH : LOW);
   digitalWrite(BUTTON_LED_2, middle_column_buttons & 0x1 ? HIGH : LOW);
@@ -131,60 +149,28 @@ void loop() {
   digitalWrite(BUTTON_LED_8, middle_column_buttons & 0x4 ? HIGH : LOW);
   digitalWrite(BUTTON_LED_9, right_column_buttons & 0x4 ? HIGH : LOW);
 
-  // int val2 = analogRead(A2);
-  // if (near(val2, 176))  {
+  // Read the fun dial and missle toggles
+  int fun_missle_dial =
+      readLadderPin(FUN_MISSLE_LADDER, 12, fun_missle_ladder_table);
 
-  // digitalWrite(4, HIGH);
-  // digitalWrite(5, HIGH);
-  // digitalWrite(6, HIGH);
-  // digitalWrite(7, HIGH);
-  // digitalWrite(8, HIGH);
-  // digitalWrite(9, HIGH);
-  // digitalWrite(A0, HIGH);
-  // digitalWrite(A1, HIGH);
-  // }
-  // else {
-  // digitalWrite(2, LOW);
-  // digitalWrite(3, LOW);
-  // digitalWrite(4, LOW);
-  // digitalWrite(5, LOW);
-  // digitalWrite(6, LOW);
-  // digitalWrite(7, LOW);
-  // digitalWrite(8, LOW);
-  // digitalWrite(9, LOW);
-  // digitalWrite(A0, LOW);
-  // digitalWrite(A1, LOW);
-  // digitalWrite(A2, LOW);
-  // digitalWrite(A3, LOW);
-  // digitalWrite(A4, LOW);
-  // digitalWrite(A5, LOW);
-  // }
+  if (fun_missle_dial & MISSILE_SWITCH_1_MASK) {
+    Serial.print("[Missle1] ");
+  }
+  if (fun_missle_dial & MISSILE_SWITCH_2_MASK) {
+    Serial.print("[Missle2] ");
+  }
+  if (fun_missle_dial & FUN_DIAL_LEFT_MASK) {
+    Serial.print("[FunDialLeft] ");
+  }
+  if (fun_missle_dial & FUN_DIAL_RIGHT_MASK) {
+    Serial.print("[FunDialRight] ");
+  }
+  Serial.println("");
 
   // Joystick.setXAxisRotation(enc1.read());
   // Joystick.setYAxisRotation(enc2.read());
-
-  // Serial.println(enc2.read());
-  // Serial.println(enc2.read());
-
-  // int camel = analogRead(A2);
-  // Serial.println(camel);
-  Serial.println(
-      readLadderPin(RIGHT_COLUMN_BUTTON_LADDER, 8, right_column_ladder_table));
-
-  // int ladder1 = readLadderPin(A3);
   // Joystick.setButton(0, ladder1 & 0x1);
   // Joystick.setButton(1, ladder1 & 0x2);
-  // Joystick.setButton(2, ladder1 & 0x4);
-
-  // int ladder2 = readLadderPin(A4);
-  // Joystick.setButton(3, ladder2 & 0x1);
-  // Joystick.setButton(4, ladder2 & 0x2);
-  // Joystick.setButton(5, ladder2 & 0x4);
-
-  // int ladder3 = readLadderPin(A5);
-  // Joystick.setButton(6, ladder3 & 0x1);
-  // Joystick.setButton(7, ladder3 & 0x2);
-  // Joystick.setButton(8, ladder3 & 0x4);
 
   delay(50);
 }
